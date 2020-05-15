f0cf67e463c605383357ec65a4137faae69aacf0
Harshit Jaiswal <Hjaiswal02@gmail.com>
2020-05-15 23:40:10 +0530

    Variables and Comments

---
+++ Basic_Scripts/variables-and-comments.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+#Hello Word Program in shell scripting 
+
+#we use hash to(#) comment
+
+#echo is used to display output on console 
+echo 'Hello Weclome to basic shell scripting'
+
+#system varible are alawys in UPPERCASE and user define varible rtry to use lowecase
+
+#few examples of system variable
+
+echo $BASH
+echo $BASH_VERSION
+echo $BASH_VERSION
+echo $PWD
+echo $HOME
+
+#examples for user define varible
+#varible name should not start with number/digit
+name=Harshit
+echo My name is $name
+val=2020
+echo $val
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+Basic_Scripts contains basic shell script which can be refer to learn basic or revice them.
